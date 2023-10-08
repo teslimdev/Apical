9536074b99c6f34dcd8afa3eadf99511461f5dff
lord-space <teslimabdullah2@gmail.com>
2023-10-08 15:03:39 -0600

    main

--- src/container/Projects/Projects.jsx
+++ src/container/Projects/Projects.jsx
@@ -1,17 +1,9 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import pic3 from '../../../src/pictures/img3.jpeg'
-import { Projectslide } from '../../components';
-import "swiper/css";
-import pic2 from "../../../src/pictures/img2.jpeg";
-import pic4 from "../../../src/pictures/img4.jpeg";
 import pic5 from "../../../src/pictures/img5.jpeg";
-import "swiper/css/pagination";
-import "swiper/css/navigation";
-import "swiper/css/effect-fade";
-import { Pagination, A11y, Autoplay, } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { EffectFade,  EffectCoverflow } from "swiper/modules";
+import pic4 from "../../../src/pictures/img4.jpeg";
+import pic2 from "../../../src/pictures/img2.jpeg";
 const Projects = () => {
   return (
     <div id="two">
@@ -20,331 +12,137 @@ const Projects = () => {
           Projects
         </h1>
         <div className="  grid  md:grid-cols-2    sl:px-0  m-auto gap-6 ">
-          <div>
-            <Link to="/">
-              <Swiper
-                modules={[
-                  Pagination,
-                  A11y,
-                  EffectFade,
-                  Autoplay,
-                  EffectCoverflow,
-                ]}
-                effect="fade"
-                loop={true}
-                speed={1500}
-                autoplay={{
-                  delay: 7000,
-                  stopOnLastSlide: false,
-                  disableOnInteraction: false,
-                  pauseOnMouseEnter: false,
-                  waitForTransition: true,
-                }}
-                a11y={{
-                  prevSlide: "true",
-                  nextSlide: "true",
-                }}
-                slidesPerView={1}
-                pagination={{
-                  el: ".swiper-pagination",
-                  clickable: true,
-                }}
-                scrollbar={{ draggable: true }}
-              >
-                <SwiperSlide>
-                  <Projectslide image={pic3}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic2}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic4}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic5}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic2}></Projectslide>
-                </SwiperSlide>
-                <div className=" ">
-                  <div className="swiper-pagination  flex gap-6  justify-center  pb-16 "></div>
+          <Link to="/">
+            <div className="relative ">
+              <div className="  ">
+                <img
+                  src={pic3}
+                  alt=""
+                  className=" w-full h-[250px] lg:h-[500px]"
+                />
+                <div className=" flex justify-center">
+                  <div className=" absolute  top-[30%]  z-10]  text-center  bg-black opacity-70  hover:bg-white text-white cursor-pointer hover:text-black py-2 px-2  lg:pr-3 sl:px-10 sl:py-4  rounded-md">
+                    <h2 className=" text-xl  lg:text-3xl sl:text-4xl pl-4 pt-6 pb-1 font-semibold">
+                      The Petronas Twin Towers
+                    </h2>
+                    <p className=" text-[0.8rem] lg:text-[0.9rem] pl-6  pt-2">
+                      HIGH LINE
+                    </p>
+                  </div>
                 </div>
-              </Swiper>
-            </Link>
-          </div>
+              </div>
+            </div>
+          </Link>
 
-          
-            <div>
-              <Link to="/">
-                 <Swiper
-                modules={[
-                  Pagination,
-                  A11y,
-                  EffectFade,
-                  Autoplay,
-                  EffectCoverflow,
-                ]}
-                effect="fade"
-                loop={true}
-                speed={1500}
-                autoplay={{
-                  delay: 7000,
-                  stopOnLastSlide: false,
-                  disableOnInteraction: false,
-                  pauseOnMouseEnter: false,
-                  waitForTransition: true,
-                }}
-                a11y={{
-                  prevSlide: "true",
-                  nextSlide: "true",
-                }}
-                slidesPerView={1}
-                pagination={{
-                  el: ".swiper-pagination",
-                  clickable: true,
-                }}
-                scrollbar={{ draggable: true }}
-              >
-                <SwiperSlide>
-                  <Projectslide image={pic2}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic3}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic4}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic5}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic4}></Projectslide>
-                </SwiperSlide>
-                <div className=" ">
-                  <div className="swiper-pagination  flex gap-6  justify-center  pb-16 "></div>
+          <Link to="/">
+            <div className="relative ">
+              <div className="  ">
+                <img
+                  src={pic4}
+                  alt=""
+                  className=" w-full h-[250px] lg:h-[500px]"
+                />
+                <div className=" flex justify-center">
+                  <div className=" absolute  top-[30%]  z-10]  text-center  bg-black opacity-70  hover:bg-white text-white cursor-pointer hover:text-black py-2 px-2  lg:pr-3 sl:px-10 sl:py-4  rounded-md">
+                    <h2 className=" text-xl  lg:text-3xl sl:text-4xl pl-4 pt-6 pb-1 font-semibold">
+                      The Petronas Twin Towers
+                    </h2>
+                    <p className=" text-[0.8rem] lg:text-[0.9rem] pl-6  pt-2">
+                      HIGH LINE
+                    </p>
+                  </div>
                 </div>
-              </Swiper>
-              </Link>
-             
+              </div>
             </div>
-          
+          </Link>
 
-            <div>
-              <Link to="/">
-                 <Swiper
-                modules={[
-                  Pagination,
-                  A11y,
-                  EffectFade,
-                  Autoplay,
-                  EffectCoverflow,
-                ]}
-                effect="fade"
-                loop={true}
-                speed={1500}
-                autoplay={{
-                  delay: 7000,
-                  stopOnLastSlide: false,
-                  disableOnInteraction: false,
-                  pauseOnMouseEnter: false,
-                  waitForTransition: true,
-                }}
-                a11y={{
-                  prevSlide: "true",
-                  nextSlide: "true",
-                }}
-                slidesPerView={1}
-                pagination={{
-                  el: ".swiper-pagination",
-                  clickable: true,
-                }}
-                scrollbar={{ draggable: true }}
-              >
-                <SwiperSlide>
-                  <Projectslide image={pic4}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic2}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic3}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic5}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic2}></Projectslide>
-                </SwiperSlide>
-                <div className=" ">
-                  <div className="swiper-pagination  flex gap-6  justify-center  pb-16 "></div>
+          <Link to="/">
+            <div className="relative ">
+              <div className="  ">
+                <img
+                  src={pic5}
+                  alt=""
+                  className=" w-full h-[250px] lg:h-[500px]"
+                />
+                <div className=" flex justify-center">
+                  <div className=" absolute  top-[30%]  z-10]  text-center  bg-black opacity-70  hover:bg-white text-white cursor-pointer hover:text-black py-2 px-2  lg:pr-3 sl:px-10 sl:py-4  rounded-md">
+                    <h2 className=" text-xl  lg:text-3xl sl:text-4xl pl-4 pt-6 pb-1 font-semibold">
+                      The Petronas Twin Towers
+                    </h2>
+                    <p className=" text-[0.8rem] lg:text-[0.9rem] pl-6  pt-2">
+                      HIGH LINE
+                    </p>
+                  </div>
                 </div>
-              </Swiper>
-              </Link>
-             
+              </div>
             </div>
-          
-            <div>
-               <Link to="/">
-                 <Swiper
-                modules={[
-                  Pagination,
-                  A11y,
-                  EffectFade,
-                  Autoplay,
-                  EffectCoverflow,
-                ]}
-                effect="fade"
-                loop={true}
-                speed={1500}
-                autoplay={{
-                  delay: 7000,
-                  stopOnLastSlide: false,
-                  disableOnInteraction: false,
-                  pauseOnMouseEnter: false,
-                  waitForTransition: true,
-                }}
-                a11y={{
-                  prevSlide: "true",
-                  nextSlide: "true",
-                }}
-                slidesPerView={1}
-                pagination={{
-                  el: ".swiper-pagination",
-                  clickable: true,
-                }}
-                scrollbar={{ draggable: true }}
-              >
-                <SwiperSlide>
-                  <Projectslide image={pic5}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic2}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic4}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic5}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic3}></Projectslide>
-                </SwiperSlide>
-                <div className=" ">
-                  <div className="swiper-pagination  flex gap-6  justify-center  pb-16 "></div>
-                </div>
-              </Swiper>
-               </Link>
-             
-            </div>
-        
+          </Link>
 
-            <div>
-              <Link to="/">
-                <Swiper
-                modules={[
-                  Pagination,
-                  A11y,
-                  EffectFade,
-                  Autoplay,
-                  EffectCoverflow,
-                ]}
-                effect="fade"
-                loop={true}
-                speed={1500}
-                autoplay={{
-                  delay: 7000,
-                  stopOnLastSlide: false,
-                  disableOnInteraction: false,
-                  pauseOnMouseEnter: false,
-                  waitForTransition: true,
-                }}
-                a11y={{
-                  prevSlide: "true",
-                  nextSlide: "true",
-                }}
-                slidesPerView={1}
-                pagination={{
-                  el: ".swiper-pagination",
-                  clickable: true,
-                }}
-                scrollbar={{ draggable: true }}
-              >
-                <SwiperSlide>
-                  <Projectslide image={pic3}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic4}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic2}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic5}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic4}></Projectslide>
-                </SwiperSlide>
-                <div className=" ">
-                  <div className="swiper-pagination  flex gap-6  justify-center  pb-16 "></div>
+          <Link to="/">
+            <div className="relative ">
+              <div className="  ">
+                <img
+                  src={pic2}
+                  alt=""
+                  className=" w-full h-[250px] lg:h-[500px]"
+                />
+                <div className=" flex justify-center">
+                  <div className=" absolute  top-[30%]  z-10]  text-center  bg-black opacity-70  hover:bg-white text-white cursor-pointer hover:text-black py-2 px-2  lg:pr-3 sl:px-10 sl:py-4  rounded-md">
+                    <h2 className=" text-xl  lg:text-3xl sl:text-4xl pl-4 pt-6 pb-1 font-semibold">
+                      The Petronas Twin Towers
+                    </h2>
+                    <p className=" text-[0.8rem] lg:text-[0.9rem] pl-6  pt-2">
+                      HIGH LINE
+                    </p>
+                  </div>
                 </div>
-              </Swiper>
-              </Link>
-              
+              </div>
             </div>
+          </Link>
+
+          <Link to="/">
+            <div className="relative ">
+              <div className="  ">
+                <img
+                  src={pic5}
+                  alt=""
+                  className=" w-full h-[250px] lg:h-[500px]"
+                />
+                <div className=" flex justify-center">
+                  <div className=" absolute  top-[30%]  z-10]  text-center  bg-black opacity-70  hover:bg-white text-white cursor-pointer hover:text-black py-2 px-2  lg:pr-3 sl:px-10 sl:py-4  rounded-md">
+                    <h2 className=" text-xl  lg:text-3xl sl:text-4xl pl-4 pt-6 pb-1 font-semibold">
+                      The Petronas Twin Towers
+                    </h2>
+                    <p className=" text-[0.8rem] lg:text-[0.9rem] pl-6  pt-2">
+                      HIGH LINE
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Link>
           
-            <div>
-               <Link to="/">
-                <Swiper
-                modules={[
-                  Pagination,
-                  A11y,
-                  EffectFade,
-                  Autoplay,
-                  EffectCoverflow,
-                ]}
-                effect="fade"
-                loop={true}
-                speed={1500}
-                autoplay={{
-                  delay: 7000,
-                  stopOnLastSlide: false,
-                  disableOnInteraction: false,
-                  pauseOnMouseEnter: false,
-                  waitForTransition: true,
-                }}
-                a11y={{
-                  prevSlide: "true",
-                  nextSlide: "true",
-                }}
-                slidesPerView={1}
-                pagination={{
-                  el: ".swiper-pagination",
-                  clickable: true,
-                }}
-                scrollbar={{ draggable: true }}
-              >
-                <SwiperSlide>
-                  <Projectslide image={pic3}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic5}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic4}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic2}></Projectslide>
-                </SwiperSlide>
-                <SwiperSlide>
-                  <Projectslide image={pic4}></Projectslide>
-                </SwiperSlide>
-                <div className=" ">
-                  <div className="swiper-pagination  flex gap-6  justify-center  pb-16 "></div>
+          <Link to="/">
+            <div className="relative ">
+              <div className="  ">
+                <img
+                  src={pic3}
+                  alt=""
+                  className=" w-full h-[250px] lg:h-[500px]"
+                />
+                <div className=" flex justify-center">
+                  <div className=" absolute  top-[30%]  z-10]  text-center  bg-black opacity-70  hover:bg-white text-white cursor-pointer hover:text-black py-2 px-2  lg:pr-3 sl:px-10 sl:py-4  rounded-md">
+                    <h2 className=" text-xl  lg:text-3xl sl:text-4xl pl-4 pt-6 pb-1 font-semibold">
+                      The Petronas Twin Towers
+                    </h2>
+                    <p className=" text-[0.8rem] lg:text-[0.9rem] pl-6  pt-2">
+                      HIGH LINE
+                    </p>
+                  </div>
                 </div>
-              </Swiper>
-               </Link>
-              
+              </div>
             </div>
+          </Link>
         </div>
       </div>
     </div>

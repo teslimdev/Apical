21c6f2a5f479a868bb3066c23c0e5f02f3977e27
lord-space <teslimabdullah2@gmail.com>
2023-10-08 13:39:19 -0600

    main

--- src/container/Projects/Projects.jsx
+++ src/container/Projects/Projects.jsx
@@ -39,6 +39,25 @@ const Projects = () => {
               </div>
             </div>
           </div>
+          <div className="relative ">
+            <div className="  ">
+              <img
+                src={pic3}
+                alt=""
+                className=" w-full h-[250px] lg:h-[500px]"
+              />
+              <div className=" flex justify-center">
+                <div className=" absolute  top-[30%]  z-10]  text-center  bg-black opacity-70  hover:bg-white text-white cursor-pointer hover:text-black py-2 px-2  lg:pr-3 sl:px-10 sl:py-4  rounded-md">
+                  <h2 className=" text-xl  lg:text-3xl sl:text-4xl pl-4 pt-6 pb-1 font-semibold">
+                    The Petronas Twin Towers
+                  </h2>
+                  <p className=" text-[0.8rem] lg:text-[0.9rem] pl-6  pt-2">
+                    HIGH LINE
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>

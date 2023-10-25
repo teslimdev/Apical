7a74607381cb78872140c5bd5f724d1143091629
lord-space <teslimabdullah2@gmail.com>
2023-10-24 18:21:35 -0600

    main

--- src/container/Products/Products.jsx
+++ src/container/Products/Products.jsx
@@ -7,7 +7,7 @@ const Products = () => {
       <Header2/>
     </section>
      <div className='  flex  justify-center '>
-      <div  className='   absolute top-[45%]  '>
+      <div  className='   absolute top-[40%]  '>
      <ul class="panels  ">
             <li className='   w-[150px]   cursor-pointer '>
                 <div  className='front bg-[#222222]  absolute     w-[150px]  text-white text-xl h-[150px] rounded-full text-center flex flex-col justify-center'>  Coming Soon</div>
